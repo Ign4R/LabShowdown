8641ded6ce42f4efa0d61b355fd8f3c16528a76b
AlenKraemer <alenkraemer@gmail.com>
2022-09-07 15:06:29 -0300

    DeathMatch Test

---
+++ Assets/_Main/Scripts/Minigames/DeathMatch.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathMatch : MonoBehaviour
+{
+
+    private int playerQuantity = 2;
+
+
+    private void Update()
+    {
+        WinCondition();
+    }
+
+
+   
+
+    public void SetPlayerQuantity(int players)
+    {
+        playerQuantity = players;
+    }
+
+    private void WinCondition()
+    {
+        if (playerQuantity == 1)
+            Debug.Log("Gano");
+    }
+
+    private void OnDieHandler()
+    {
+        playerQuantity--;
+    }
+
+    private void OnEnable()
+    {
+        StatsController.OnDie += OnDieHandler;
+    }
+
+    private void OnDisable()
+    {
+        StatsController.OnDie -= OnDieHandler;
+    }
+}
