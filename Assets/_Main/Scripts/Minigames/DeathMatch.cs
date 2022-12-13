cb9703f69265f14b674bc57164037bea03798088
Nacho <69459950+ignisR@users.noreply.github.com>
2022-12-13 14:29:39 -0300

    fix destroy weapon

--- Assets/_Main/Scripts/Minigames/DeathMatch.cs
+++ Assets/_Main/Scripts/Minigames/DeathMatch.cs
@@ -102,9 +102,9 @@ public class DeathMatch : MonoBehaviour
     {
         //TODO: PRIORIDAD RB VELOCITY 0. TIMER
 
-        players[playerIndex].GetComponent<PlayerModel>().DropWeapon();
-        players[playerIndex].GetComponent<PlayerModel>().Weapon.DestroyWeapon();
         players[playerIndex].transform.position = respawnPoints[playerIndex].transform.position;
+        players[playerIndex].GetComponent<PlayerModel>().Weapon.DestroyWeapon();
+        players[playerIndex].GetComponent<PlayerModel>().WeaponIsNull();
     }
 
     private void OnDieHandler(int playerIndex)
